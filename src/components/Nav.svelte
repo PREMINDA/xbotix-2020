c62b88f338583f46d66c5351939df3907068baf1
PREMINDA <premindabandara0777@gmail.com>
2020-09-23 00:56:12 +0530

    Add basic nav bar

--- src/components/Nav.svelte
+++ src/components/Nav.svelte
@@ -1,60 +1,20 @@
-<script>
-	export let segment;
-</script>
-
-<style>
-	nav {
-		border-bottom: 1px solid rgba(255,62,0,0.1);
-		font-weight: 300;
-		padding: 0 1em;
-	}
-
-	ul {
-		margin: 0;
-		padding: 0;
-	}
-
-	/* clearfix */
-	ul::after {
-		content: '';
-		display: block;
-		clear: both;
-	}
-
-	li {
-		display: block;
-		float: left;
-	}
-
-	[aria-current] {
-		position: relative;
-		display: inline-block;
-	}
-
-	[aria-current]::after {
-		position: absolute;
-		content: '';
-		width: calc(100% - 1em);
-		height: 2px;
-		background-color: rgb(255,62,0);
-		display: block;
-		bottom: -1px;
-	}
-
-	a {
-		text-decoration: none;
-		padding: 1em 0.5em;
-		display: block;
-	}
-</style>
-
-<nav>
-	<ul>
-		<li><a aria-current="{segment === undefined ? 'page' : undefined}" href=".">home</a></li>
-		<li><a aria-current="{segment === 'about' ? 'page' : undefined}" href="about">about</a></li>
-
-		<!-- for the blog link, we're using rel=prefetch so that Sapper prefetches
-		     the blog data when we hover over the link or tap it on a touchscreen -->
-		<li><a rel=prefetch aria-current="{segment === 'blog' ? 'page' : undefined}" href="blog">blog</a></li>
-	</ul>
+<nav class="flex flex-row bg-gray-200 justify-between">
+  <img class="h-16" src="./xlogo.png" alt="iyghui" />
+  <div class="flex flex-row">
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4
+        rounded mx-3 my-3">
+      Home
+    </button>
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4
+        rounded mx-3 my-3">
+      About
+    </button>
+    <button
+      class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4
+        rounded mx-3 my-3">
+      Gallery
+    </button>
+  </div>
 </nav>
